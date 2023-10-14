f045c7b6afdb683f5fe884a4f3971636ad6ad7cc
mustafa-sel <108630102+mustafa-sel@users.noreply.github.com>
2023-10-14 15:51:43 +0300

    Add files via upload

---
+++ react/weather_api/src/App.js
@@ -0,0 +1,10 @@
+import Weather from './components/Weather.jsx';
+import './components/style.css'
+
+export default function App() {
+    return (
+        <div>
+            <Weather />
+        </div>
+    )
+}
