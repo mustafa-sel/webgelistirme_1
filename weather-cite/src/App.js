267c60dd91612889456dc89cc55debd4b8e8558e
mustafa-sel <108630102+mustafa-sel@users.noreply.github.com>
2023-11-01 12:52:29 +0300

    Add files via upload

---
+++ weather-cite/src/App.js
@@ -0,0 +1,13 @@
+import NavBarLinks from './Navbar/NavBarLinks';
+import './App.css'
+import Home from './Home/Home'
+function App() {
+  return (
+    <div className="App">
+      <NavBarLinks/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-cite/src/Weather.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Weather() {
+  return (
+    <div>
+        ASD
+    </div>
+  )
+}
+
+export default Weather
